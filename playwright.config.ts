32231c46bb12d4a1e00c9e70aea71083456a3d8a
Ryan Duffy <ryan@replay.io>
2022-03-11 14:05:32 -0800

    Add replay configuration to playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
 import {PlaywrightTestConfig, devices} from '@playwright/test';
+import {devices as replayDevices} from '@recordreplay/playwright-config';
 
 const config: PlaywrightTestConfig = {
+  workers: 1,
   use: {
     launchOptions: {
       ignoreDefaultArgs: ['--hide-scrollbars'],
@@ -13,6 +15,18 @@ const config: PlaywrightTestConfig = {
     reuseExistingServer: !process.env.CI,
   },
   projects: [
+    {
+      name: 'replay-firefox',
+      use: {
+        ...replayDevices["Replay Firefox"] as any
+      }
+    },
+    {
+      name: 'replay-chromium',
+      use: {
+        ...replayDevices["Replay Chromium"] as any
+      }
+    },
     {
       name: 'chromium',
       use: {...devices['Desktop Chrome']},
